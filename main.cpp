acc9168c5a168af86f24c56856af73056eaf4c8d
Mikhail Zverlov <goeaturdog@googlemail.com>
2016-06-08 21:24:57 +0200

    mac commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//platz für Kommentare
+//platz fuer Kommentare
 #include <iostream>
 #include <string>
 #include "Complex.h"
@@ -7,18 +7,8 @@ using namespace std;
 
 int main()
 {
-	auto zahl1 = Complex(4, -3);
-	auto zahl2 = Complex(2, 1);
-	auto zahl3 = zahl1 + zahl2;
-	cout << zahl1;
-	double Value;
-	cout << endl << "Zahl eingeben" << endl;
-	cin >> Value;
-	cout << endl << Value << endl;
-
-
-
-	int i;
-	cin >> i;
+    //nix aber sync
+    
+    
 }
 
